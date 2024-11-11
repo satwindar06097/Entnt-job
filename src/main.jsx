d6795f84ead91152a1861aa98d1988bd85a9dba3
Satveer <satwindar06097@gmail.com>
2024-11-11 22:06:40 +0530

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App.jsx";
 import { Provider } from "react-redux";
-import Store from "./Store.js";
+import Store from "./store.js";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={Store}>
